61d083fb6dd7bfea2f62374ae0b49822ebb0beba
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-25 15:07:21 -0400

    commit tree 199759193e8bea4da25b793b3fab5219432bd521

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  const newArray = []
+  for (let element of sourceArray){
+    newArray.push(element * -1)
+  } 
+  return newArray
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.slice()
+}
+
+function mapToDouble(sourceArray){
+  const newArray = []
+  for (let element of sourceArray){
+    newArray.push(element * 2)
+  } 
+  return newArray
+}
+
+function mapToSquare(sourceArray){
+  const newArray = []
+  for (let element of sourceArray){
+    newArray.push(element ** 2)
+  } 
+  return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+  for (let element of sourceArray){
+    startingPoint += element
+  }
+  return startingPoint
+}
+
+function reduceToAllTrue(sourceArray){
+  for (let element of sourceArray){
+    if (!element){
+      return false
+    }
+  }
+  return true
+}
+
+
+function reduceToAnyTrue(sourceArray){
+  for (let element of sourceArray){
+    if (element){
+      return true
+    }
+  }
+  return false
+}
